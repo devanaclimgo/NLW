9506c53d6c42c358604438f6ee9cca00e0745342
devanaclimgo <anaclimgo@gmail.com>
2025-07-12 13:15:53 -0300

    build: edited server file to host configurations

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,3 +5,17 @@ import {
   ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
+
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173'
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
