1233dd05d85e7ac2b2acf618dd0c7324650a9b0e
devanaclimgo <anaclimgo@gmail.com>
2025-07-13 21:10:31 -0300

    feat: server updated to all rooms

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,11 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts' 
 import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts' 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -24,5 +27,8 @@ app.get('/health', () => {
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
