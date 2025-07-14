e4e283a3fdcbfccc01f7eb6fbc79b6bb70ad2064
devanaclimgo <anaclimgo@gmail.com>
2025-07-14 15:03:38 -0300

    feat: added missing imports, create-question-route and upload-audio-route

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,8 @@ import { env } from './env.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -26,5 +28,7 @@ app.get('/health', () => {
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
